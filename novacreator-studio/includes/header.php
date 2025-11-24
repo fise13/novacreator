049d2e702b2d635d3a3ce7ee623bc2c307938e75
fise13 <victhewise@icloud.com>
2025-11-24 19:56:12 +0500

    Добавлен viewport-fit=cover для корректной работы safe area insets на iPhone

--- novacreator-studio/includes/header.php
+++ novacreator-studio/includes/header.php
@@ -11,8 +11,8 @@ if (session_status() === PHP_SESSION_NONE) {
 <html lang="ru" itemscope itemtype="https://schema.org/WebSite">
 <head>
     <meta charset="UTF-8">
-    <!-- Viewport оптимизирован для мобильных устройств -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+    <!-- Viewport оптимизирован для мобильных устройств с поддержкой safe area insets -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes, viewport-fit=cover">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Предотвращение автоматического определения телефонных номеров на iOS -->
     <meta name="format-detection" content="telephone=yes">
